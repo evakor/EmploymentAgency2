49b957f34972fb95f609b17ad447b6d3d39e3ae3
Eva <evarizm32@gmail.com>
2024-04-04 20:28:09 +0300

    Added the view More button in the home page

--- server.js
+++ server.js
@@ -16,7 +16,19 @@ app.get('/employee', (req, res) => {
     res.render('pages/employeeProfile');
 });
 app.get('/employer', (req, res) => {
-  res.render('pages/employerProfile');
+  res.render('pages/employerProfile', {
+    firstname:"Maria",
+    lastname: "Rousou",
+    email: "marpap@gmail.com",
+    phone1: "6981234567",
+    phone2: null,
+    address: "Ermou 33",
+    region: "Athens",
+    comp_name: "Kafe Mpampis",
+    comp_desc: "To kafe Mpampis einai ena poly omorfo kafe stous propodes ths akropolis.",
+    job_title: ["Kafetzis", "Mprizas", "Mhxanikos Autokiniton"],
+    job_desc: "Mia fora kai enan kairo htan h kokkinoskoufitsa kai efage ton kako ton lyko telos."
+  });
 });
 
 app.get('/jobs', (req, res) => {

d97435fb8291d2edfd414e91ce7832dc55718f21
ChristosK17 <christoskiokakis@gmail.com>
2024-04-16 19:25:47 +0300

    Added CRUD endpoints to the server

--- server.js
+++ server.js
@@ -18,6 +18,97 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // REST Routes
 
+// JOB endpoints
+app.post('/job/', (req, res) => {
+  res.send('Job created');
+});
+
+app.get('/job/:id', (req, res) => {
+  res.send(`Retrieve job with ID ${req.params.id}`);
+});
+
+app.get('/jobs/', (req, res) => {
+  res.send('All jobs retrieved');
+});
+
+app.put('/job/:id', (req, res) => {
+  res.send(`Update job with ID ${req.params.id}`);
+});
+
+app.delete('/job/:id', (req, res) => {
+  res.send(`Job with ID ${req.params.id} deleted`);
+});
+
+// EMPLOYEE endpoints
+
+app.post('/employee/', (req, res) => {
+  res.send('Employee created');
+});
+
+app.get('/employee/:id', (req, res) => {
+  res.send(`Retrieve employee with ID ${req.params.id}`);
+});
+
+app.get('/employees/', (req, res) => {
+  res.send('All employees retrieved');
+});
+
+app.put('/employee/:id', (req, res) => {
+  res.send(`Update employee with ID ${req.params.id}`);
+});
+
+app.delete('/employee/:id', (req, res) => {
+  res.send(`Employee with ID ${req.params.id} deleted`);
+});
+
+
+// EMPLOYER endpoints
+
+app.post('/employer/', (req, res) => {
+  res.send('Employer created');
+});
+
+app.get('/employer/:id', (req, res) => {
+  res.send(`Retrieve employer with ID ${req.params.id}`);
+});
+
+app.get('/employers/', (req, res) => {
+  res.send('All employers retrieved');
+});
+
+app.put('/employer/:id', (req, res) => {
+  res.send(`Update employer with ID ${req.params.id}`);
+});
+
+app.delete('/employer/:id', (req, res) => {
+  res.send(`Employer with ID ${req.params.id} deleted`);
+});
+
+
+// APPLIES endpoints
+
+app.post('/application/', (req, res) => {
+  res.send('Application created');
+});
+
+app.get('/application/:id', (req, res) => {
+  res.send(`Retrieve application with ID ${req.params.id}`);
+});
+
+app.get('/applications/', (req, res) => {
+  res.send('All applications retrieved');
+});
+
+app.put('/application/:id', (req, res) => {
+  res.send(`Update application with ID ${req.params.id}`);
+});
+
+app.delete('/application/:id', (req, res) => {
+  res.send(`Application with ID ${req.params.id} deleted`);
+});
+
+
+
 // View Routes
 app.get('/', (req, res) => {
     res.render('home');

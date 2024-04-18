eba06d4416dbc4906d95b2ec084b887f52c08195
Eva <evarizm32@gmail.com>
2024-04-18 23:41:46 +0300

    Edit data base and routes

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("navSidePanel").style.width = "190px";
-  }
-  
-  function closeNav() {
-    document.getElementById("navSidePanel").style.width = "0";
-  }
